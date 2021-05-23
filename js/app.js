03a695f077d7a98599717d456d0de35ad8cf9273
beefoo <brian@youaremyjoy.org>
2021-05-23 12:25:36 -0400

    add shaders

--- js/app.js
+++ js/app.js
@@ -1,5 +1,76 @@
 'use strict';
 
+var MaterialVertexShader = `
+  precision mediump float;
+
+  uniform float positionTransitionPct;
+  uniform float alphaTransitionPct;
+
+  attribute vec2 uvOffset;
+  attribute float alpha;
+  attribute float alphaDest;
+  attribute vec3 scale;
+  attribute vec3 translate;
+  attribute vec3 translateDest;
+  attribute vec3 actualSize;
+  attribute vec3 color;
+
+  varying vec2 vUv;
+  varying vec3 vColor;
+  varying vec3 vUidColor;
+  varying float vAlpha;
+
+  #define PI 3.14159
+  void main() {
+    float pPct = positionTransitionPct;
+    if (pPct > 1.0) pPct = 1.0;
+
+    vec3 p = mix( translate, translateDest, pPct );
+    vec4 mvPosition = modelViewMatrix * vec4(p, 1.0);
+    mvPosition.xyz += position * actualSize;
+    vUv = uvOffset.xy + uv * actualSize.xy / scale.xy;
+
+    float aPct = alphaTransitionPct;
+    if (aPct > 1.0) aPct = 1.0;
+    vAlpha = (alphaDest-alpha) * aPct + alpha;
+
+    // move the point far away if alpha zero
+    if (vAlpha <= 0.0) {
+      p = vec3(-999999., -999999., -999999.);
+    }
+
+    vColor = color;
+
+    gl_Position = projectionMatrix * mvPosition;
+  }
+`;
+
+var MaterialFragmentShader = `
+  precision mediump float;
+
+  uniform sampler2D map;
+  uniform vec3 fogColor;
+  uniform float fogDistance;
+
+  varying vec2 vUv;
+  varying vec3 vColor;
+  varying float vAlpha;
+
+  void main() {
+  if( length( vColor ) < .1 )discard;
+
+  //fog
+  float depth = gl_FragCoord.z / gl_FragCoord.w;
+  float d = clamp( 0., 1., pow( depth * ( 1./fogDistance ), 2. ) );
+  if( d >= 1. ) discard;
+
+  vec4 diffuseColor = texture2D(map, vUv);
+  gl_FragColor = diffuseColor * vec4(vColor, 1.0);
+  gl_FragColor.rgb = mix( gl_FragColor.rgb, fogColor, d );
+  gl_FragColor.a = vAlpha;
+}
+`;
+
 var App = (function() {
 
   function App(config) {
@@ -9,7 +80,13 @@ var App = (function() {
       number: 500000,
       cameraDistance: 4000,
       audioFile: 'audio/octave/tap-resonant.mp3',
-      waitAudioMs: 40 // wait this long before playing sound again
+      waitAudioMs: 40, // wait this long before playing sound again
+      textureFile: 'img/crowd_texture.png',
+      textureWidth: 2048,
+      textureHeight: 2048,
+      cellWidth: 256,
+      cellHeight: 256,
+      cellCount: 42
     };
     var q = queryParams();
     this.opt = _.extend({}, defaults, config, q);
@@ -55,6 +132,7 @@ var App = (function() {
   App.prototype.init = function(){
     var _this = this;
     this.$el = $('#app');
+    this.$scene = $('#scene');
 
     this.loadSound();
     this.loadSlider();
@@ -83,7 +161,16 @@ var App = (function() {
   };
 
   App.prototype.loadScene = function(){
-
+    var $el = $('#scene');
+    var w = $el.width();
+    var h = $el.height();
+    scene = new THREE.Scene();
+    camera = new THREE.PerspectiveCamera( 75, w / h, 0.0001, 8000 );
+    renderer = new THREE.WebGLRenderer({ antialias: true });
+    renderer.setPixelRatio( window.devicePixelRatio );
+    renderer.setClearColor( 0x000000, 0.0 );
+    renderer.setSize(w, h);
+    $el.append(renderer.domElement);
   };
 
   App.prototype.loadSound = function(){
